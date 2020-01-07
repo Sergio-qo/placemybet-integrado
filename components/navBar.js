f6ac012bb7f4623abc9810d3cd6d7a555258a7f5
Sergio-qo <sergio.qo000@gmail.com>
2020-01-07 19:37:30 +0100

    subo screens

--- components/navBar.js
+++ components/navBar.js
@@ -17,12 +17,16 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import {FontAwesome5} from '@expo/vector-icons'
+
 class navBar extends React.Component {
     render() {
         return (
             <View style={{flex: 1}}>
                 <TouchableOpacity style={{alignItems: "flex-end", margin: 16}}
-                 onPress={this.props.navigation.openDrawer}/>
+                 onPress={this.props.navigation.openDrawer}>
+                     <FontAwesome5 name="bars" size={24} color="black"/>
+                 </TouchableOpacity>
             </View>
         );
     }
